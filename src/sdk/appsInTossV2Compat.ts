d4cb2000fbb5ac38d80253f8e268d5560f9d0348
LeeTheY <peter20011148@gmail.com>
2026-08-11 00:41:10 +0900

    chore: 앱인토스 SDK를 v3에서 v2로 마이그레이션

---
+++ src/sdk/appsInTossV2Compat.ts
@@ -0,0 +1,10 @@
+import {
+  SafeAreaInsets,
+  setScreenAwakeMode,
+} from '#apps-in-toss-sdk-v2';
+
+export const SafeArea = SafeAreaInsets;
+
+export const Screen = {
+  setAwakeMode: setScreenAwakeMode,
+};
