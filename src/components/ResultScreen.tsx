00929c7ea273bbd2c73885f1ecc1ee09f68b16ae
hansol-dev <hihikhs@gmail.com>
2026-08-12 00:03:43 +0900

    style: separate ranking note and boost result confetti

--- src/components/ResultScreen.tsx
+++ src/components/ResultScreen.tsx
@@ -148,6 +148,17 @@ export function ResultScreen({
         <i />
         <i />
         <i />
+        <i />
+        <i />
+        <i />
+        <i />
+        <i />
+        <i />
+        <i />
+        <i />
+        <i />
+        <i />
+        <i />
       </div>
       <header className="result-record-heading">
         <p className="eyebrow">오늘의 기록</p>
