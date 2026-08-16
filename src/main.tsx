4034f5f35f77b0bd19070c8ad6e17c09eb4dc656
unknown <comkj89@gmail.com>
2026-08-16 22:26:42 +0900

    fix:높이 700px이하 롤백 이상 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './utils/homeViewportClass';
 import './styles/globals.css';
+import './styles/home-responsive.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
